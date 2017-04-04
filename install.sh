1a7a45773c1bd7967a1cf36851923e5c5edb11d2
Zunware <jose@zunware.com>
2017-04-03 21:46:32 -0500

    Implemented create users from YAML

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 # Installs npm for virtualenv
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xvz
-$ cd node-v*
-$ ./configure --prefix=$VIRTUAL_ENV
-$ make install
+#curl http://nodejs.org/dist/node-latest.tar.gz | tar xvz
+#$ cd node-v*
+#$ ./configure --prefix=$VIRTUAL_ENV
+#$ make install
 
 # Install and update protractor
 npm install -g protractor
 webdriver-manager update
 
 # Pix Diff
-npm install -g pix-diff
+npm install pix-diff
+# YAML
+npm install js-yaml
 
 # Installs the hello command
 pip install --editable .
